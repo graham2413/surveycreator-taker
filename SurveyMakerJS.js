d0da657f3334c7a42ff7f602cf87e03eefdd828f
Graham Norris <christian.graham.norris@gmail.com>
2022-04-07 15:07:28 -0500

    fixed all methods

--- SurveyMakerJS.js
+++ SurveyMakerJS.js
@@ -11,14 +11,26 @@ function typeSelectOn(that) {
     else if(that.value=="multChoice"){
         document.getElementById("multChoices").style.display = "block";
         document.getElementById("selAllChoices").style.display = "none";
+        document.getElementById("oneToFive").style.display = "none";
+        document.getElementById("yesNo").style.display = "none";
     }
     else if(that.value=="selAll"){
-        document.getElementById("selAllChoices").style.display = "block";
+        document.getElementById("selAll").style.display = "block";
         document.getElementById("multChoices").style.display = "none";
+        document.getElementById("oneToFive").style.display = "none";
+        document.getElementById("yesNo").style.display = "none";
+    }
+    else if(that.value=="oneToFive"){
+        document.getElementById("oneToFive").style.display = "block";
+        document.getElementById("multChoices").style.display = "none";
+        document.getElementById("selAll").style.display = "none";
+        document.getElementById("yesNo").style.display = "none";
     }
     else{
+        document.getElementById("yesNo").style.display = "block";
         document.getElementById("multChoices").style.display = "none";
-        document.getElementById("selAllChoices").style.display = "none";
+        document.getElementById("selAll").style.display = "none";
+        document.getElementById("oneToFive").style.display = "none";
     }
 }
 
@@ -41,18 +53,22 @@ function addQuestion(){
     document.getElementById("label1").textContent = "Question";
 
     var node = document.createElement("li");
+
+    //Question text below
     var text = document.getElementById("question").value;
     var textnode = document.createTextNode(text);
     node.appendChild(textnode);
+    console.log(text);
+
+  //type of question below
+    var type = document.getElementById("qType").value;
+    console.log(type);
+
+
     document.getElementById("questionList").appendChild(node);
 
     incNum++;
 
-    var type = document.getElementById("qType").value;
-    
-    console.log(text);
-    console.log(type);
-    
     //Pass this data as well as the data of who is making (userID)
     createSingleQuestion();
 
@@ -92,4 +108,39 @@ function addFields(){
         // Append a line break 
         container.appendChild(document.createElement("br"));
     }
+} 
+function addCheckBox(){
+    
+    // Generate a dynamic number of inputs
+    var number = document.getElementById("selNum").value;
+    console.log(number);
+    // Get the element where the inputs will be added to
+    var container = document.getElementById("Selcontainer");
+    // Remove every children it had before
+    while (container.hasChildNodes()) {
+        container.removeChild(container.lastChild);
+    }
+    for (i=0;i<number;i++){
+        
+        // Append a node with a random text
+        container.appendChild(document.createTextNode("Choice " + (i+1)));
+        // Create an <input> element, set its type and name attributes
+        var input = document.createElement("INPUT");
+        input.setAttribute("type", "checkbox");
+        container.appendChild(input);
+        // Append a line break 
+        container.appendChild(document.createElement("br"));
+    }
+} 
+
+
+
+//databse stuff with data
+function subFunc(){
+
+ var form  = document.getElementById('formform');
+
+console.log(form.elements)
+
+
 }
